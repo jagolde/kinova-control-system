811fe59b2c1b9fe6d2efba9a53f772dd901441d5
GrantSRechtin <gsullivanr@icloud.com>
2026-04-16 10:44:12 -0400

    pseudo

--- main.py
+++ main.py
@@ -4,9 +4,40 @@ import time
 class Main(BaseApp):
 
     def start(self):
-        pass
-    
-    def loop(self):
+        # Go to home
+        # Get initial camera frame once at home -- FUNCTION
+        #   Camera Calibration
+        # Save positions for Pour Cup & Fill Cup in world & camera frame -- FUNCTION
+        #   World 0,0,0 at arm base
+        # Set state to WAITING
+
+        pass        
+        
+    def loop(self):        
+        # if state == ACTING
+        #   do actions until list is complete
+        #   Set state to waiting
+        # elif state == WAITING
+        #   command input method  --  Button / Screen / Etc...
+        #   command -> add list of actions
+        #       Go to first cup
+        #           Trajectory generation
+        #           Movement
+        #       Pick up cup
+        #       Move up
+        #           Trajectory generation
+        #           Movement
+        #       Go to pour location
+        #           Trajectory generation
+        #           Movement
+        #       Pour
+        #       Straighten
+        #       Move back
+        #           Trajectory generation
+        #           Movement
+        #       Put down bup
+        #   Set state to acting
+        
         pass
 
 
