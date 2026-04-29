63095da99d24d2647b04762b6d96bfdc66034c69
kerwin4 <kieran.erwin@gmail.com>
2026-04-29 14:23:12 -0400

    updated example code

--- example.py
+++ example.py
@@ -7,43 +7,65 @@ class Main(BaseApp):
         
     def start(self):
         self.home = False
-        self.target, _ = calc_forward_kinematics([0.79, 6.11, 1.48, 1.4, 6.11, 1.57])
-        
-        # self.angles = calc_numerical_ik(self.target,joint_values)
-        # ee = EndEffector()
-        # ee.x = 0.25
-        # ee.y = 0.25
-        # ee.z = 0.25
-        # ee.rotx = 0
-        # ee.roty = 0
-        # ee.rotz = pi/2
-        self.angles = calc_numerical_ik(self.target,joint_values)
-        
-    def loop(self):  
-        # print(self.target.x)
-        # print(self.target.y)
-        # print(self.target.z)     
-        # print(self.target.rotx)
-        # print(self.target.roty)
-        # print(self.target.rotz)
-        # print(self.angles)
-        is_7DOF = False
-        
-        if(is_7DOF is None):
-            raise ValueError("If you are using the big robot set is_7DOF to true. If you are using the small robot set is_7DOF to false")
-        
-        if(is_7DOF):
-            HOME_POSITION = np.array([3.10, 5.99, 3.35, 2.54, 0.14, 4.92, 1.41])
-            next_position = np.array([2.67, 5.47, 2.85, 1.92, 0.14, 4.92, 1.41])
-            
+
+        ee = EndEffector()
+        # above cup
+        ee.x = 0.25
+        ee.y = -0.35
+        ee.z = 0.25
+        ee.rotx = 0
+        ee.roty = pi/2
+        ee.rotz = 0
+        self.angles = calc_inverse_kinematics(ee,joint_values)
+        # down to cup
+        ee.x = 0.25
+        ee.y = -0.35
+        ee.z = 0.025
+        ee.rotx = 0
+        ee.roty = pi/2
+        ee.rotz = 0
+        self.angles1 = calc_inverse_kinematics(ee,joint_values)
+        # move in to cup
+        ee.x = 0.35
+        ee.y = -0.35
+        ee.z = 0.025
+        ee.rotx = 0
+        ee.roty = pi/2
+        ee.rotz = 0
+        self.angles2 = calc_inverse_kinematics(ee,joint_values)
+        # lift can over cup
+        ee.x = 0.2
+        ee.y = -0.1
+        ee.z = 0.2
+        ee.rotx = 0
+        ee.roty = pi/2
+        ee.rotz = 0
+        self.angles3 = calc_inverse_kinematics(ee,joint_values)
+        # rotate cup
+        self.angles4 = self.angles3.copy()
+        if self.angles4[5] + 2.5 > 2.6:
+            self.angles4[5] -= 2.5
         else:
-            HOME_POSITION = np.array([1.75, 5.76, 2.18, 2.44, 4.54, 0.0])
-            next_position = np.array([0.79, 6.11, 1.48, 1.4, 6.11, 1.57])
-            target_pos = self.angles
+            self.angles4[5] += 2.5
+        
+    def loop(self):
+        #HOME_POSITION = np.array([1.75, 5.76, 2.18, 2.44, 4.54, 0.0])
+        HOME_POSITION = np.array([0, 0, 0, 0, 0, 0])
+        next_position = np.array([0.79, 6.11, 1.48, 1.4, 6.11, 1.57])
         if(self.home):
-            self.kinova_robot.set_joint_angles(next_position, gripper_percentage=100)
+            self.kinova_robot.set_joint_angles(self.angles, gripper_percentage=0)
             time.sleep(1)
-            self.kinova_robot.set_joint_angles(target_pos, gripper_percentage=100)
+            self.kinova_robot.set_joint_angles(self.angles1, gripper_percentage=0)
+            time.sleep(1)
+            self.kinova_robot.set_joint_angles(self.angles2, gripper_percentage=60)
+            time.sleep(1)
+            self.kinova_robot.set_joint_angles(self.angles3, gripper_percentage=60)
+            time.sleep(1)
+            self.kinova_robot.set_joint_angles(self.angles4, gripper_percentage=60)
+            time.sleep(1)
+            self.kinova_robot.set_joint_angles(self.angles3, gripper_percentage=60)
+            time.sleep(1)
+            self.kinova_robot.set_joint_angles(self.angles2, gripper_percentage=0)
             time.sleep(1)
             # self.home = False
 
