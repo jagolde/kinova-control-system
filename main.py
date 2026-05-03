2697cd2d3ec0b1044190980b3fd4c77390ad76aa
GrantSRechtin <gsullivanr@icloud.com>
2026-05-03 03:26:04 -0400

    Readme and final cleanup

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import numpy as np
 import pyrealsense2 as rs
 import matplotlib.pyplot as plt
 import cv2 as cv
-import tkinter as tk
 from math import pi
 
 from camera import RealsenseCamera
 from camera import SimCamera
 
-from ui_helpers import make_button
+from ui_helpers import tkinter_setup
 from kinematics_helpers import (
     EndEffector,
     calc_forward_kinematics,
@@ -148,7 +147,7 @@ class Main(BaseApp):
                 self.state = "WAITING"
 
         elif self.state == "WAITING":
-            self.tkinter_setup()
+            tkinter_setup(self)
 
 
     def _add_pour_sequence(self, cup):
@@ -265,55 +264,6 @@ class Main(BaseApp):
 
         self.state = "ACTING"
         root.destroy()
-
-    def tkinter_setup(self):
-        root = tk.Tk()
-        root.geometry("800x600")
-
-        from PIL import Image, ImageTk
-        bg_img = ImageTk.PhotoImage(Image.open("menu_background.jpg").resize((800, 600)))
-
-        canvas = tk.Canvas(root, width=800, height=600, highlightthickness=0)
-        canvas.place(x=0, y=0, relwidth=1, relheight=1)
-        canvas.create_image(0, 0, anchor="nw", image=bg_img)
-        canvas.image = bg_img
-
-        # Buttons
-        cup_1_button = make_button(
-            root=root,
-            command=lambda *args: self.pour_cup_button(root, [self.pour_cup_1]),
-            text="Water",
-            width=25,
-            height=1,
-            wraplength=0
-        )
-        cup_2_button = make_button(
-            root=root,
-            command=lambda *args: self.pour_cup_button(root, [self.pour_cup_2]),
-            text="Water",
-            width=25,
-            height=1,
-            wraplength=0
-        )
-        mix_button = make_button(
-            root=root,
-            command=lambda *args: self.pour_cup_button(root, [self.pour_cup_1, self.pour_cup_2]),
-            text="Mixed (so still just water)",
-            width=25,
-            height=1,
-            wraplength=0
-        )
-
-        canvas.create_text(400, 295, text="Pour Cup 1", font=("Arial", 8), fill="gray")
-        canvas.create_text(400, 355, text="Pour Cup 2", font=("Arial", 8), fill="gray")
-
-        cup_1_button.place(relx=0.5, rely=0.45, anchor="center")
-        cup_2_button.place(relx=0.5, rely=0.55, anchor="center")
-        mix_button.place(relx=0.5,   rely=0.65, anchor="center")
-
-        root.mainloop()
-
-
 
 if __name__ == "__main__":
     final_project = Main(
